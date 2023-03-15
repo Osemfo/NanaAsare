0c7854521917dc9e97fab54f19aab2e9823233a9
Osemfo <canyinaku@gmail.com>
2023-03-15 21:59:43 +0000

    printf

---
+++ C_Programming/printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main-Entry point of he program
+ * Return: Always zero
+ * This is a program that prints something on the screen
+ */
+
+int main(void)
+{
+	printf("Hello world!\n");
+
+	return (0);
+}
